40f11f7d27c76e9d4a215f6dfc6f236ce54ccf29
Elfueros <odidiperez@gmail.com>
2022-03-11 02:54:07 +0000

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,9 @@
  * main-Printing with puts
  */
 int main(void)
+/*
+ * main- Printing with puts
+ */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,9 @@
  * main - Printing with printf
  */
 int main(void)
+/*
+ * main- Printing with printf
+ */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);

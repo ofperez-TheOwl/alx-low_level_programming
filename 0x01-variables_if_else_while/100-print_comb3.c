3582602583ae73f34ef2d0d475cab8b67f8c4e4d
Elfueros <odidiperez@gmail.com>
2022-03-12 02:54:02 +0000

    advanced tasks

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,5 +6,25 @@
  */
 int main(void)
 {
-	
+	int i = '0';
+	int j = '1';
+
+	putchar(i);
+	putchar(j);
+	while (i <= '9' && j <= '9')
+	{
+		if (i == j || i > j)
+		{
+			i = i + 1;
+			j = j + 1;
+		}
+		putchar(',');
+		putchar(' ');
+		putchar(i);
+		putchar(j);
+		i = i + 1;
+		j = j + 1;
+	}
+	putchar('\n');
+	return (0);
 }

b4554ad2acb014e70f8ac1e62bf0c4c2439f379a
Elfueros <odidiperez@gmail.com>
2022-03-16 03:01:29 +0000

    easiest tasks review

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,7 +7,7 @@
  * Return: Always 0
  */
 
-int print_to_98(int n)
+void print_to_98(int n)
 {
 	int i = n;
 
@@ -33,7 +33,4 @@ int print_to_98(int n)
 		}
 		_putchar('\n');
 	}
-
-	return (0);
 }
-
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -19,4 +19,6 @@ void jack_bauer(void);
 
 int add(int, int);
 
+void print_to_98(int n);
+
 

85881f1aed46dbda5ef88faef354eb71f3f460ce
Elfueros <odidiperez@gmail.com>
2022-03-17 01:27:24 +0000

    advanced tasks review

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,49 +2,57 @@
 
 /**
  * print_times_table - print multiplication table up to n
- * @n: integer argument
+ * @n: integer
+ *
+ * Return: nothing
  */
 
 void print_times_table(int n)
 {
-	int row;
-	int column;
-	int product;
-
-	if (n >= 0 && n <= 15)
+	int i = 0;
+	int j = 0;
+	int k;
+       
+	if (n <= 15 && n >= 0)
 	{
-		for (row = 0; row <= n; row++)
+		while (i <= n)/* lines*/
 		{
-			for (column = 0; column <= n; column++)
+			while (j <= n)/* columns*/
 			{
-				product = (row * column);
-				if (column == 0)
-					_putchar('0' + product);
-				else
+				k = i * j;
+				if (k == 0 && j == 0)/*first column*/
+				{
+					_putchar('0');
+				}
+				else if (k > 99)/*three digit*/
 				{
 					_putchar(',');
 					_putchar(' ');
-					if (product <= 9)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar('0' + product);
-					}
-					else if (product > 9 && product < 100)
-					{
-						_putchar(' ');
-						_putchar('0' + (product / 10));
-						_putchar('0' + (product % 10));
-					}
-					else if (product >= 100)
-					{
-						_putchar('0' + (product / 100));
-						_putchar('0' + ((product / 10) % 10));
-						_putchar('0' + (product % 10));
-					}
+					_putchar('0' + (k / 100));
+					_putchar('0' + ((k % 100) / 10));
+					_putchar('0' + (k % 10));
 				}
+				else if (k > 9 && k < 100)/*double digit*/
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (k / 10));
+					_putchar('0' + (k % 10));
+				}
+				else if (k >= 0 && k <= 9)/*one digit except first column*/
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + k);
+				}
+				j = j + 1;
 			}
-			_putchar('\n');
+			_putchar('\n');/* next line*/
+			j = 0;
+			i = i + 1;
 		}
 	}
 }

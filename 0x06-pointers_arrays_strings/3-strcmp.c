dc5d07e1152fc1f3efdc972959b480b79c85a84b
Elfueros <odidiperez@gmail.com>
2022-03-25 22:01:38 +0000

    reviews

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - calculate the length of a string
- * @s: pointer to a string
- *
- * Return: integer(value of length)
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
-}
-
-/**
  * *_strcmp - compare two strings
  * @s1: pointer to first string
  * @s2: pointer to second string
@@ -31,36 +11,13 @@ int _strlen(char *s)
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int k = 0;
-	int lens1 = _strlen(s1);
-	int lens2 = _strlen(s2);
 
-	if (lens1 >= lens2)
+	while ( *s1 != '\n' && *s1 == *s2)
 	{
-		while (s1[i] != '\0')
-		{
-			if (s1[i] == s2[i] && k == 0)
-				k = 0;
-			else if (s1[i] > s2[i])
-				k++;
-			else if (s1[i] < s2[i])
-				k--;
-			i++;
-		}
+		s1++;
+		s2++;
 	}
-	else
-	{
-		while (s2[i] != '\0')
-		{
-			if (s1[i] == s2[i] && k == 0)
-				k = 0;
-			else if (s1[i] > s2[i])
-				k++;
-			else if (s1[i] < s2[i])
-				k--;
-			i++;
-		}
-	}
+	i = ((*s1 + 0) - (*s2 + 0))
 
-	return (k);
+	return (i);
 }

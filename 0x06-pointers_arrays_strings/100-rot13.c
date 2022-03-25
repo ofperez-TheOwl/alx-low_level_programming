0cacdc987db25950e0a15523ce327cda93349d12
Elfueros <odidiperez@gmail.com>
2022-03-25 02:51:06 +0000

    advanced

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * *rot13 - encode using rot13
+ * @s: pointer to the string
+ *
+ * Return: pointer to encoded string
+ */
+
+char *rot13(char *s)
+{
+
+	int from[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		     'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		     'W', 'X', 'Y', 'Z'};
+	int to[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		     'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		     'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		     'J', 'K', 'L', 'M'};
+	int i;
+	int j;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		i = 0;
+		while (from[i] != '\0' && s[j] != from[i]) /*loop through rot13 arr*/
+			i++;
+		if (s[j] == from[i]) /*coding*/
+			s[j] = to[i];
+	}
+
+	return (s);
+}

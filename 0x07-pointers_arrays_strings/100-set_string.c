b75dea055f5d4f5a65309c9e96710327de8b072d
Elfueros <odidiperez@gmail.com>
2022-03-29 02:03:02 +0000

    advanced tasks

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - set the value of a pointer to a character
+ * @s: double pointer to string;
+ * @to: pointer to string;
+ *
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_chessboard - print a chessboard 8*8

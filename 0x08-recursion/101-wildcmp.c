1daabe8ac0672c672b1680325ec653d4e42774ca
Elfueros <odidiperez@gmail.com>
2022-04-01 02:40:05 +0000

    advanced tasks reviewed

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -23,7 +23,12 @@ int wildcmp(char *s1, char *s2)
 	else if (*(s1 + 1) == '\0' && *(s1 + 1) != *(s2 + 1)
 			&& *s2 == '*' && *(s2 + 1) != '\0')
 		return (0);
+	else if (*(s1 + 1) == '\0' && *s2 == '*' && *(s2 + 1) == '*')
+		return (0);
+
 	/* recursive instructions */
+	else if (*(s1 + 1) != '\0' && *s2 == '*' && *(s2 + 1) == '*')
+		return (wildcmp(s1, s2 + 1));
 	else if (*s1 == *(s2 + 1) && *s2 == '*' && *(s2 + 1) != '\0')
 		return (wildcmp(s1, s2 + 1));
 	else if (*(s1 + 1) == *(s2 + 1) && *s2 == '*' && *(s2 + 1) != '\0')

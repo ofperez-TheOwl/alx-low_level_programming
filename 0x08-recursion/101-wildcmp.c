bbeaa8f8b06cd0f4b0c7577b68ecbd9bd32d28e0
Elfueros <odidiperez@gmail.com>
2022-04-01 18:52:28 +0000

    advanced tasks reviewed

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -18,6 +18,8 @@ int wild_test(char *s1, char *s2, char *nextchar)
 	if (*s1 == '\0' && *s2 != '\0')
 		return (0);
 	/* recursive instructions */
+	if (*s1 == '\0' && *s2 == '*')
+		return (wild_test(s1, s2 + 1, s2 + 1));
 	if (*s2 == '*')
 		return (wild_test(s1, s2 + 1, s2 + 1));
 	else if (*s1 == *s2)
@@ -52,29 +54,3 @@ int wildcmp(char *s1, char *s2)
 	else
 		return (0);
 }
-/**
- * if (*s1 != *s2 && (*s1 == '\0' || *s2 == '\0') && *s2 != '*')
-		return (0);
-	if (*s1 == *s2 && *(s1 + 1) == '\0' && *(s2 + 1) == '\0' && *s2 != '*')
-		return (1);
-	if (*(s1 + 1) == '\0' && *s2 == '*' && *(s2 + 1) == '\0')
-		return (1);
-	if (*(s1 + 1) != '\0' && *s2 == '*' && *(s2 + 1) == '\0')
-		return (0);
-	if (*(s1 + 1) == '\0' && *(s1 + 1) != *(s2 + 1)
-			&& *s2 == '*' && *(s2 + 1) != '\0')
-		return (0);
-	if (*(s1 + 1) == '\0' && *s2 == '*' && *(s2 + 1) == '*')
-		return (0);
-
-	* recursive instructions *
-	if (*(s1 + 1) != '\0' && *s2 == '*' && *(s2 + 1) == '*')
-		return (wildcmp(s1, s2 + 1));
-	if (*s1 == *(s2 + 1) && *s2 == '*' && *(s2 + 1) != '\0')
-		return (wildcmp(s1, s2 + 1));
-	if (*(s1 + 1) == *(s2 + 1) && *s2 == '*' && *(s2 + 1) != '\0')
-		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s1 == *s2 && *(s1 + 1) != '\0' && *(s2 + 1) != '\0' && *s2 != '*')
-		return (wildcmp(s1 + 1, s2 + 1));
-}
-*/

1a153f340377099bd7575e80ba8a04a6f86b5647
Elfueros <odidiperez@gmail.com>
2022-04-06 21:51:39 +0000

    review task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,17 +32,18 @@ int _strlen(char *s)
 
 char *str_concat(char *s1, char *s2)
 {
+	int len1, len2, i, j;
+	char *conc;
+
 	/* exlusion */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	int len1 = _strlen(s1);
-	int len2 = _strlen(s2);
-	int i;
-	int j;
-	char *conc = malloc(sizeof(char) * (len1 + len2 + 1));
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	conc = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	/*validation */
 	if (conc == NULL)

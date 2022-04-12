2592a73799410be603193611256d9816ca69a09d
Elfueros <odidiperez@gmail.com>
2022-04-12 02:00:34 +0000

    preprocessor task completed

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * prints name of file
+ *
+ * Return: always 1
+ * TheOwl
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (1);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,7 @@
+***C Preprocessing***
+
+* This is y first task about preprocessing in C.
+
+* It's mainly focusing in the use of macros and how to avoid misusing them.
+
+***Go for code!***

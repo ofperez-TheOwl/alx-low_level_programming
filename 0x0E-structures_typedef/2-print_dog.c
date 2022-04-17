69f80e2d190a831f8b424115b23b3e6e037e7c4c
Elfueros <odidiperez@gmail.com>
2022-04-17 21:38:12 +0000

    review

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -23,7 +23,7 @@ void print_dog(struct dog *d)
 	if (d->age == 0)
 		printf("Age: (nil)\n");
 	else
-		printf("Age: %.6f\n", d->age);
+		printf("Age: %f\n", d->age);
 	/* print dog's owner */
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");

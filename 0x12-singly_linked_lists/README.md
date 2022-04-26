5ad227249f903930b5333d2350f3db668dece17e
Elfueros <odidiperez@gmail.com>
2022-04-26 02:35:57 +0000

    Singly linked lists: requirement settled

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,7 @@
+***Singly link list***
+
+* This is the first project of the section Data structures and algorithms of the ALX low level programming lectures.
+
+* This project is about linked list and it is my first meeting with this concept.
+
+***Go for code!***
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,42 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+size_t print_list(const list_t *h);
+
+size_t list_len(const list_t *h);
+
+list_t *add_node(list_t **head, const char *str);
+
+list_t *add_node_end(list_t **head, const char *str);
+
+void free_list(list_t *head);
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+#endif

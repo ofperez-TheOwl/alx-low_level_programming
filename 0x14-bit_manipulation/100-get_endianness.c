b4ed25a710b843bfcff6fe21b90f3c9463d5810c
Elfueros <odidiperez@gmail.com>
2022-04-30 01:32:39 +0000

    advanced task reviewed

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * get_endianess - gets the endianess of the machine
+ * get_endianness - gets the endianess of the machine
  *
  * Return: int; 0 if big-endian or 1 if little_endian
  * TheOwl
  */
-int get_endianess(void)
+int get_endianness(void)
 {
 	int i = 1;
 	char *c = (char *)(&i);

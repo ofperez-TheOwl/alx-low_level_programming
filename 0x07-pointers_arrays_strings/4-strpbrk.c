203585581ed34b1144daea77cb9a1302f54b1d50
Elfueros <odidiperez@gmail.com>
2022-05-21 13:47:04 +0000

    review of task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,28 +1,26 @@
 #include "main.h"
+#define NULL 0
 
 /**
- * _strpbrk - search a tring for a character from a set of character
- * @s: pointer to string; string to sreen
+ * _strpbrk - search a string for a character from a set of character
+ * @s: pointer to string; string to screen
  * @accept: pointer to string; set of character to search for
  *
  * Return: pointer to first occurence of character or NULL if none
+ * TheOwl
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
-	int j;
+	int i, j;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
-				return (s + i);
-			if (*(s + i + 1) == '\0' && *(accept + j + 1) == '\0' &&
-					*(s + i) != *(accept + j))
-				return (s + i + 1);
+				return (&s[i]);
 		}
 	}
-	return ('\0');
+	return (NULL);
 }

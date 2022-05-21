1d0e8e65318f92612aa4e5da36410e5d4dfc0ea7
Elfueros <odidiperez@gmail.com>
2022-05-21 14:51:34 +0000

    review of task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,20 +9,19 @@
  * Return: integer; 1 if identical or 0 if not
  * TheOwl
  */
-
 int wild_test(char *s1, char *s2, char *nextchar)
 {
 	/* base case */
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	if (*s1 == '\0' && *s2 != '\0')
-		return (0);
 	/* recursive instructions */
 	if (*s1 == '\0' && *s2 == '*')
 		return (wild_test(s1, s2 + 1, s2 + 1));
+	if (*s1 == '\0' && *s2 != '\0') /* base case */
+		return (0);
 	if (*s2 == '*')
 		return (wild_test(s1, s2 + 1, s2 + 1));
-	else if (*s1 == *s2)
+	if (*s1 == *s2)
 		return (wild_test(s1 + 1, s2 + 1, nextchar));
 	else
 		return (wild_test(s1 + 1, nextchar, nextchar));
@@ -36,19 +35,16 @@ int wild_test(char *s1, char *s2, char *nextchar)
  * Return: integer; 1 if identical or 0 if not
  * TheOwl
  */
-
 int wildcmp(char *s1, char *s2)
 {
-	/* recursive exclusion */
-	if (*s2 == '*')
-		return (wild_test(s1, s2 + 1, s2 + 1));
-
 	/* base case */
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 	if (*s1 != *s2)
 		return (0);
-	else if (*s1 == '\0' && *s2 == '\0')
-		return (1);
 	/* recursive instructions */
+	if (*s2 == '*')
+		return (wild_test(s1, s2 + 1, s2 + 1));
 	else if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	else

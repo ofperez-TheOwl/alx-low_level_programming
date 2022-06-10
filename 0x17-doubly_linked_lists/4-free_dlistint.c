a91be5fc0e1b231c1d718d63d0188fe090524bb2
Elfueros <odidiperez@gmail.com>
2022-06-10 01:51:53 +0000

    task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - frees all elements of a dlistint_t list
+ * @head: pointer to dlistint_t; first node of list
+ *
+ * Return: nothing
+ * TheOwl
+ */
+void free_dlistint(dlistint_t *head)
+{
+	/* base case */
+	if (head == NULL)
+	{
+		free(head);
+		return;
+	}
+	if (head->next == NULL)
+	{
+		free(head->next);
+		return;
+	}
+	/* recursive instructions */
+	free_dlistint(head->next);
+	free(head->next);
+	if (head->prev == NULL)
+	{
+		free(head->prev);
+		free(head);
+	}
+}

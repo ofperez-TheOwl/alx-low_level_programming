dc545af48fbd836f218954b1be34a609fc9b182a
Elfueros <odidiperez@gmail.com>
2022-07-02 15:29:56 +0000

    review task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -14,7 +14,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 
 	if (size == 0)
 		return (0);
-	
+
 	i = hash_djb2(key);
 	return (i % size);
 }
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: pointer to hash_table_t; the hash table
+ * @key: const char; key of the element to add
+ * @value: const char; value of the element to add
+ *
+ * Return: int; 1 if success and 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+
+	/* get the index of key */
+	/* create a copy of value */
+	/* access index of key */
+	/* check if index is free */

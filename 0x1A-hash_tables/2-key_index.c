a0811fa07f9e47fe740426a5cf8d104bd8cf9153
Elfueros <odidiperez@gmail.com>
2022-07-02 00:12:01 +0000

    task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+ * key_index - finds the index of a key
+ * @key: const unsigned char; key to evaluate
+ * @size: unsigned long int; size of the table
+ *
+ * Return: unsigned long int; index of the key
+ * TheOwl
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int i;
+
+	if (size == 0)
+		return (0);
+	
+	i = hash_djb2(key);
+	return (i % size)
+}

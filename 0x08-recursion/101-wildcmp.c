79dccb7eb2390487cd773359aa24b4fd0d3b7b88
Elfueros <odidiperez@gmail.com>
2022-07-08 21:34:50 +0000

    review task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,26 @@
 #include "main.h"
 
+
+/**
+ * occurence_cleaner - check occurrence of a char
+ * @c: char; char to check for
+ * @str: pointer to string; stringto process
+ * @n: int; counter
+ *
+ * Return: int; position of last c or 0 if not found
+ * TheOwl
+ */
+int occurence_cleaner(char *str, char c, int n)
+{
+	/* base case */
+	if (*(str + n) == '\0')
+		return (0);
+	if (c != *(str + n))
+		return (n);
+	/* recursive instruction */
+	return (occurence_cleaner(str, c, n + 1));
+}
+
 /**
  * occurence_check - check occurrence of a char
  * @c: char; char to check for
@@ -41,6 +62,8 @@ int wild_test(char *s1, char *s2, int n)
 	{
 		if (*s1 != '\0' && *s2 == '\0')
 			return (1);
+		/* clean multiple occurence of the same char after '*' */
+		s2 = s2 + occurence_cleaner(s2, *s2, 1);
 		if (*s1 != '\0' && *s2 != '*' && *s2 != '\0')
 		{
 			n = occurence_check(s1, *s2, n);
--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -1,38 +1,19 @@
-#include "main.h"
 #include <stdio.h>
 
+int wildcmp(char *, char *);
+
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int r;
+	int r;
 
-    r = wildcmp("main.c", "*.c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m*a*i*n*.*c*");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "main.c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m*c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "ma********************************c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "*");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "***");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m.*c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "**.*c");
-    printf("%d\n", r);
-    r = wildcmp("main-main.c", "ma*in.c");
-    printf("%d\n", r);
-    r = wildcmp("main", "main*d");
-    printf("%d\n", r);
-    r = wildcmp("abc", "*b");
-    printf("%d\n", r);
-    return (0);
+	r = wildcmp("school-school.c", "sch*ool.c");
+	printf("%d\n", r);
+	r = wildcmp("main-main.c", "ma*in.c");
+	printf("%d\n", r);
+	return (0);
 }

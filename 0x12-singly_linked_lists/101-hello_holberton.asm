0ff03fcdd39c0216aabc2effdd2f80784d82c21c
Elfueros <odidiperez@gmail.com>
2022-07-08 22:29:49 +0000

    review task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,21 @@
 ;This is my very first program in assembler language
 ;It is very simple print hello world
 ;TheOwl
+global main
+
 section .text
-	global _start		;entry point declaration
-_start				;entry point
-	mov rdx,len		;length of message
-	mov rcx,msg		;messsage
-	mov rbx,1		;file descriptor output of the program
-	mov rax,4		;system call number (sys_write)?
-	int 0x80		;call kernel(?)
 
-	mov rax,1		;system call number(sys_exit)?
-	int 0x80		;call kernel
+main:
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, msg
+  mov rdx, msglen
+  syscall
 
-section .data
-msg db 'Hello, Holberton', 0xa	;string to be printed
-len equ $ - msg			;length of string
+  mov rax, 60
+  mov rdi, 0
+  syscall
+
+section .rodata
+  msg: db "Hello, Holberton", 10
+  msglen: equ $ - msg

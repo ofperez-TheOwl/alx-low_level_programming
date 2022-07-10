55ddcadda599dad7c4527bac65116225b3dd77f6
Elfueros <odidiperez@gmail.com>
2022-07-10 01:41:31 +0000

    review task 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -12,7 +12,7 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *tmp_prev = NULL, *tmp_next = (*head)->next;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (NULL);
 	if ((*head)->next == NULL)
 		return (*head);

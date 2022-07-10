8149294937e10e9c29e88c4837ab095d9f052bf6
Elfueros <odidiperez@gmail.com>
2022-07-10 01:36:52 +0000

    task 11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses linked list
+ * @head: listint_t pointer to pointer; list to reverse
+ *
+ * Return: listint_t pointer; first node of new list
+ * TheOwl
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *tmp_prev = NULL, *tmp_next = (*head)->next;
+
+	if (*head == NULL)
+		return (NULL);
+	if ((*head)->next == NULL)
+		return (*head);
+	/* traversing and reodering */
+	while (*head != NULL)
+	{
+		(*head)->next = tmp_prev;
+		if (tmp_next == NULL)
+			return (*head);
+		tmp_prev = *head;
+		*head = tmp_next;
+		tmp_next = tmp_next->next;
+	}
+	return (NULL);
+}

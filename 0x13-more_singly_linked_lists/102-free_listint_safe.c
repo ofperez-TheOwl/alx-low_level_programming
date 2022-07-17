706b8417d71b08b91a389e2b40a6f8c17046e97a
Elfueros <odidiperez@gmail.com>
2022-07-17 02:02:00 +0000

    task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -46,44 +46,6 @@ void free_if_loop(listint_t *head, size_t count, size_t ref)
 	free_if_loop(head->next, count + 1, ref);
 	free(head);
 }
-/*
-**
- * loop_finder - finds a loop in a list of listint_t
- * @head: constant listint_t pointer; list
- *
- * Return: loop_r; number of element before loop and junction node
- *
- * Desscription: use Floyd Circle finding algorithm
- * TheOwl
- *
-loop_r loop_finder(const listint_t *head)
-{
-	loop_r result = {NULL, 0};
-	const listint_t *tmp_slow = head, *tmp_fast = head;
-
-	if (head->next == NULL)
-		return (result);
-	while (tmp_slow && tmp_fast && tmp_fast->next)
-	{
-		tmp_slow = tmp_slow->next;
-		tmp_fast = (tmp_fast->next)->next;
-		result.len++;
-		if (tmp_slow == tmp_fast)
-		{
-			result.node = tmp_slow;
-			tmp_slow = head;
-			while (tmp_slow != tmp_fast)
-			{
-				tmp_slow = tmp_slow->next;
-				tmp_fast = tmp_fast->next;
-				result.len++;
-			}
-			return (result);
-		}
-	}
-	return (result);
-}
-*/
 
 /**
  * free_listint_safe - frees a list of listint_t even if there is a loop

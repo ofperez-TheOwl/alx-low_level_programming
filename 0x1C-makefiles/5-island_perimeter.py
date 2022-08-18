ccab5ff85dd8aa16d720fcaf11db2bc4f06ecba7
ofperez-TheOwl <odidiperez@gmail.com>
2022-08-18 16:59:38 +0000

    review of task 6

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""5-island_perimeter
+program that returns the perimeter of an island
+"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid
+    Args:
+        grid(list of list) : representation of island
+    """

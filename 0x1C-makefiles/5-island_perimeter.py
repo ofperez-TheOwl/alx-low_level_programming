6a3ef493c506107290eb9e11a6928197f9614beb
ofperez-TheOwl <odidiperez@gmail.com>
2022-08-18 21:22:51 +0000

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,3 +9,16 @@ def island_perimeter(grid):
     Args:
         grid(list of list) : representation of island
     """
+    perimeter = 0
+    for i in range(0, len(grid)):
+        for j in range(0, len(gird[i])):
+            if (grid[i][j] != 0):
+                if (i + 1 < len(grid) and grid[i + 1][j] == 0):
+                    perimeter += 1
+                if (i != 0 and grid[i - 1][j] == 0):
+                    perimeter += 1
+                if (j + 1 < len(grid[i]) and grid[i][j + 1] == 0):
+                    perimeter += 1
+                if (j != 0 and grid[i][j - 1] == 0):
+                    perimeter += 1
+    return (perimeter)
